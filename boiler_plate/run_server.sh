fb11aae6968a0e9d2630f8d42d18af2970c694b2
Manu <dhandedhan@gmail.com>
2017-07-14 17:38:12 +0530

    brunch fix

--- boiler_plate/run_server.sh
+++ boiler_plate/run_server.sh
@@ -1,4 +1,6 @@
 git pull
-boiler_ui/node_modules/brunch/bin/brunch build --production
+cd boiler_ui
+node_modules/brunch/bin/brunch build 
+cd ..
 python manage.py collectstatic
 python manage.py runserver 0.0.0.0:6549
